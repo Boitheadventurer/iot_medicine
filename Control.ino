e95e07893afcd133d714f7713e5b36ab6e9b8812
Boitheadventurer <165980784+Boitheadventurer@users.noreply.github.com>
2024-05-15 13:07:40 +0700

    Add files via upload

---
+++ Control.ino
@@ -0,0 +1,301 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <Servo.h>
+#include <String.h>
+#include <TridentTD_LineNotify.h>
+//#include <Wire.h>
+Servo myservo;
+
+#define sensor D5
+int val;
+int x;
+int i;
+
+/*2.4G*/
+const char* ssid = "CTN floor 2 teacher"; // Wi-Fi SSID
+const char* password = "ctnphrae"; // Wi-Fi password
+
+const char* LINE_TOKEN = "QIChSQJdNBnK08VtgMSjRPhikDUQVGP3ikBPexwgQFU";
+
+// URL by file PHP (http://(IP4)/(folder)(file.php))
+
+String POSTURL = "http://192.168.10.14/Medic/post.php"; 
+String GETURL  = "http://192.168.10.14/Medic/get.php";
+//String POSTURL = "http://medicinectn2555.000webhostapp.com/post.php"; 
+//String GETURL  = "http://medicinectn2555.000webhostapp.com/get.php";
+
+WiFiClient client;
+HTTPClient http;
+int httpCode;
+
+//***GET***//
+int UserID; // UserID
+String time_get;
+
+// Time meal
+String bf_time;
+String lun_time;
+String dn_time;
+String bb_time;
+
+//Medic take follow ID
+int bf_medic[4];
+int lun_medic[4];
+int dn_medic[4];
+int bb_medic[4];
+int medic_send[4];
+
+int meal; // Meal for send data_tb
+String status; // Status medicine success
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(sensor, INPUT);
+  myservo.attach(D4);
+  //Wire.begin(); // Start the I2C
+  connectWiFi(); // Connect WiFi function
+
+  //condition_GET();
+  http.begin(client, GETURL);
+  httpCode = http.GET();
+  String payload = http.getString();
+  Serial.print("HTTP_GET Response Code: "); Serial.println(httpCode);
+  Serial.println(LINE.getVersion());
+  LINE.setToken(LINE_TOKEN);
+  LINE.notify("SETUP COMPLETE");
+  //Serial.print("payload: ");    Serial.println(payload);
+  Serial.println("----------SETUP CONTROL.INO READY--------------");
+  delay(250);
+}
+
+void loop() {
+  if(WiFi.status() != WL_CONNECTED) {
+    connectWiFi();
+  }
+  val = digitalRead(sensor);
+  condition_GET();
+  if (bf_time == "" || lun_time == "" || dn_time == "" || bb_time == "") {
+    Serial.println("***");
+    Serial.print("ERROR CONNECT SERVER HTTP : "); Serial.println(httpCode);
+    Serial.println("***");
+    while (bf_time == "" || lun_time == "" || dn_time == "" || bb_time == "") {
+      condition_GET();
+    }
+    Serial.println("SERVER CONNECTED");
+  }
+  condition_CHECK();
+  delay(150);
+}
+
+// ConnectWiFi
+void connectWiFi() {
+  WiFi.mode(WIFI_OFF);
+  delay(1000);
+  //This line hides the viewing of ESP as wifi hotspot
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("connected to : ");  Serial.println(ssid);
+  Serial.print("IP address: ");     Serial.println(WiFi.localIP());
+}
+
+// Servo rotate counter-clockwise full-speed
+void rotateservo() {
+  myservo.writeMicroseconds(1000);
+}
+
+// Servo rotation stopped
+void stopservo() {
+  myservo.writeMicroseconds(1500);
+}
+
+// Condition_GET
+void condition_GET() {
+  http.begin(client, GETURL);
+  httpCode = http.GET();
+  String payload = http.getString();
+  //Serial.print("HTTP_GET Response Code: "); Serial.println(httpCode);
+  //Serial.print("payload : ");               Serial.println(payload);
+
+  // GET UserID from INPUT Website
+  int index = payload.indexOf("UserID=");
+    String userIDString = payload.substring(index + 7);
+    UserID = userIDString.toInt();
+
+  // GET RealTime from PHP
+  int t = payload.indexOf("time=");
+    String tt = payload.substring(t + 5);
+    int substr_tt = tt.indexOf("UserID=");
+    tt = tt.substring(0, substr_tt);
+  time_get = tt;
+  
+  // GET bf_time
+  int bf = payload.indexOf("bf_time=");
+    String bf_timeString = payload.substring(bf + 8);
+    int substr_bf = bf_timeString.indexOf("bf_medic");
+    bf_timeString = bf_timeString.substring(0, substr_bf);
+  bf_time = bf_timeString;
+  int bf1 = payload.indexOf("bf_medic1=");
+    String bf1_String = payload.substring(bf1 + 10);
+    bf_medic[0] = bf1_String.toInt();
+  int bf2 = payload.indexOf("bf_medic2=");
+    String bf2_String = payload.substring(bf2 + 10);
+    bf_medic[1] = bf2_String.toInt();
+  int bf3 = payload.indexOf("bf_medic3=");
+    String bf3_String = payload.substring(bf3 + 10);
+    bf_medic[2] = bf3_String.toInt();
+  int bf4 = payload.indexOf("bf_medic4=");
+    String bf4_String = payload.substring(bf4 + 10);
+    bf_medic[3] = bf4_String.toInt();
+
+  // GET lun_time
+  int lun = payload.indexOf("lun_time=");
+    String lun_timeString = payload.substring(lun + 9);
+    int substr_lun = lun_timeString.indexOf("lun_medic");
+    lun_timeString = lun_timeString.substring(0, substr_lun);
+  lun_time = lun_timeString;
+  int lun1 = payload.indexOf("lun_medic1=");
+    String lun1_String = payload.substring(lun1 + 11);
+    lun_medic[0] = lun1_String.toInt();
+  int lun2 = payload.indexOf("lun_medic2=");
+    String lun2_String = payload.substring(lun2 + 11);
+    lun_medic[1] = lun2_String.toInt();
+  int lun3 = payload.indexOf("lun_medic3=");
+    String lun3_String = payload.substring(lun3 + 11);
+    lun_medic[2] = lun3_String.toInt();
+  int lun4 = payload.indexOf("lun_medic4=");
+    String lun4_String = payload.substring(lun4 + 11);
+    lun_medic[3] = lun4_String.toInt();
+
+  // GET dn_time
+  int dn = payload.indexOf("dn_time=");
+    String dn_timeString = payload.substring(dn + 8);
+    int substr_dn = dn_timeString.indexOf("dn_medic");
+    dn_timeString = dn_timeString.substring(0, substr_dn);
+  dn_time = dn_timeString;
+  int dn1 = payload.indexOf("dn_medic1=");
+    String dn1_String = payload.substring(dn1 + 10);
+    dn_medic[0] = dn1_String.toInt();
+  int dn2 = payload.indexOf("dn_medic2=");
+    String dn2_String = payload.substring(dn2 + 10);
+    dn_medic[1] = dn2_String.toInt();
+  int dn3 = payload.indexOf("dn_medic3=");
+    String dn3_String = payload.substring(dn3 + 10);
+    dn_medic[2] = dn3_String.toInt();
+  int dn4 = payload.indexOf("dn_medic4=");
+    String dn4_String = payload.substring(dn4 + 10);
+    dn_medic[3] = dn4_String.toInt();
+
+  // GET bb_time by UserID
+  int bb = payload.indexOf("bb_time=");
+    String bb_timeString = payload.substring(bb + 8);
+    int substr_bb = bb_timeString.indexOf("bb_medic");
+    bb_timeString = bb_timeString.substring(0, substr_bb);
+  bb_time = bb_timeString;
+  int bb1 = payload.indexOf("bb_medic1=");
+    String bb1_String = payload.substring(bb1 + 10);
+    bb_medic[0] = bb1_String.toInt();
+  int bb2 = payload.indexOf("bb_medic2=");
+    String bb2_String = payload.substring(bb2 + 10);
+    bb_medic[1] = bb2_String.toInt();
+  int bb3 = payload.indexOf("bb_medic3=");
+    String bb3_String = payload.substring(bb3 + 10);
+    bb_medic[2] = bb3_String.toInt();
+  int bb4 = payload.indexOf("bb_medic4=");
+    String bb4_String = payload.substring(bb4 + 10);
+    bb_medic[3] = bb4_String.toInt();
+}
+
+// Condition in condition_CHECK
+void condition_CHECK_send() {
+  x = 1; // x == 1
+  Serial.println("Time to medicine!");
+  LINE.notify("ถึงเวลาที่กำหนดจ่ายยาแล้ว!");
+  rotateservo();
+  delay(100);   //delay(40);
+  stopservo();
+  delay(1000);
+  x++; // x == 2
+}
+
+// Condition_CHECK data before POST
+void condition_CHECK() {
+  // Check if it's time for any meal
+  if (time_get == bf_time) {
+      meal = 1;
+      medic_send[0] = bf_medic[0];  //Serial.println(medic_send[0]);
+      medic_send[1] = bf_medic[1];  //Serial.println(medic_send[1]);
+      medic_send[2] = bf_medic[2];  //Serial.println(medic_send[2]);
+      medic_send[3] = bf_medic[3];  //Serial.println(medic_send[3]);
+      condition_CHECK_send();
+  } else if (time_get == lun_time) {
+      meal = 2;
+      medic_send[0] = lun_medic[0]; //Serial.println(medic_send[0]);
+      medic_send[1] = lun_medic[1]; //Serial.println(medic_send[1]);
+      medic_send[2] = lun_medic[2]; //Serial.println(medic_send[2]);
+      medic_send[3] = lun_medic[3]; //Serial.println(medic_send[3]);
+      condition_CHECK_send();
+  } else if (time_get == dn_time) {
+      meal = 3;
+      medic_send[0] = dn_medic[0];  //Serial.println(medic_send[0]);
+      medic_send[1] = dn_medic[1];  //Serial.println(medic_send[1]);
+      medic_send[2] = dn_medic[2];  //Serial.println(medic_send[2]);
+      medic_send[3] = dn_medic[3];  //Serial.println(medic_send[3]);
+      condition_CHECK_send();
+  } else if (time_get == bb_time) {
+      meal = 4;
+      medic_send[0] = bb_medic[0];  //Serial.println(medic_send[0]);
+      medic_send[1] = bb_medic[1];  //Serial.println(medic_send[1]);
+      medic_send[2] = bb_medic[2];  //Serial.println(medic_send[2]);
+      medic_send[3] = bb_medic[3];  //Serial.println(medic_send[3]);
+      condition_CHECK_send();
+  }
+  int k;
+  if (x == 2 && val == 0) {
+    delay(100);
+    k++;
+    if (k == 1000) {
+      Serial.println("Take medicine failed!");
+      status = "'failed'";
+      LINE.notify("ผู้ป่วยไม่ได้รับยาในเวลาที่กำหนด!");
+      LINE.notifyPicture(time_get, "https://www.shareicon.net/data/256x256/2015/09/15/101562_incorrect_512x512.png");
+      condition_POST();
+      k = 0;
+    }
+  } else if (x == 2 && val == 1) {
+    Serial.println("Take medicine success!");
+    status = "'success'";
+    LINE.notify("ผู้ป่วยได้รับยาในเวลาที่กำหนด สำเร็จ!");
+    LINE.notifyPicture(time_get, "https://cdn-icons-png.flaticon.com/512/4436/4436481.png");
+    condition_POST();
+    k = 0;
+  }
+}
+
+// Condition_POST after condition_CHECK
+void condition_POST() {
+  String postData = "UserID=" + String(UserID) + "&meal=" + String(meal) + 
+  "&medic_send1=" + String(medic_send[0]) + "&medic_send2=" + String(medic_send[1]) + 
+  "&medic_send3=" + String(medic_send[2]) + "&medic_send4=" + String(medic_send[3]) + 
+  "&status=" + String(status);
+  WiFiClient client;
+  HTTPClient http;
+  http.begin(client, POSTURL);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded"); // ***Send data
+  int httpCode = http.POST(postData);
+  String payload = http.getString();
+  Serial.print("HTTP_POST Response Code: "); Serial.println(httpCode);
+  Serial.print("POSTURL : "); Serial.println(POSTURL); 
+  Serial.print("Data: ");     Serial.println(postData);
+    //Serial.print("payload : "); Serial.println(payload);
+  if (httpCode == 200) {
+    //Serial.println("Send data success!");
+    Serial.println("--------------------COMPLETE----------------------");
+    delay(100);
+  }
+  x++; // x == 3
+}
---
+++ Output.ino
@@ -0,0 +1,230 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+/*2.4G*/
+const char* ssid = "CTN floor 2 teacher"; // Wi-Fi SSID
+const char* password = "ctnphrae"; // Wi-Fi password
+
+// URL by file PHP (http://(IP4)/(folder)(file.php))
+
+String GETURL = "http://192.168.10.14/Medic/tft.php";
+//String GETURL = "http://medicinectn2555.000webhostapp.com/tft.php";
+
+WiFiClient client;
+HTTPClient http;
+int httpCode;
+
+#define TFT_CS   D8
+#define TFT_RST  D4
+#define TFT_DC   D3
+
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+//7-color (eInk) displays:
+//White , Black , Red , Green , Blue , Yellow and Orange.
+
+String time_get;
+int warn;
+
+String Name;
+String BF;
+String LUN;
+String DN;
+String BB;
+
+int st_bf;
+int st_lun;
+int st_dn;
+int st_bb;
+
+void setup() {
+  Serial.begin(115200);
+  tft.initR(INITR_BLACKTAB);
+  tft.fillScreen(ST77XX_BLACK);
+  connectWiFi();
+  http.begin(client, GETURL);
+  httpCode = http.GET();
+  String payload = http.getString();
+  Serial.print("HTTP Response Code: "); Serial.println(httpCode);
+    //Serial.print("payload: ");           Serial.println(payload);
+  Serial.println("------------SETUP OUTPUT.INO READY-----------");
+  tft.fillScreen(ST77XX_BLACK);
+}
+
+void loop() {
+  if(WiFi.status() != WL_CONNECTED) {
+    connectWiFi();
+  }
+  condition_GET_tft();
+  layout();
+  tft_text();
+
+  delay(1000 * 10);
+  tft.fillScreen(ST77XX_BLACK);
+}
+
+//ConnectWiFi
+void connectWiFi() {
+  WiFi.mode(WIFI_OFF);
+  delay(1000);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    tft.setCursor(5, 65);
+    tft.setTextSize(2);
+    tft.setTextColor(ST77XX_WHITE);
+    tft.print("Connecting");
+  }
+  Serial.print("connected to : "); Serial.println(ssid);
+  Serial.print("IP address: "); Serial.println(WiFi.localIP());
+}
+
+//Condition GET tft and meal
+void condition_GET_tft() {
+  WiFiClient client;
+  HTTPClient http;
+  http.begin(client, GETURL);
+  int httpCode = http.GET();
+  String payload = http.getString();
+
+  // Time
+  int t = payload.indexOf("time=");
+  String tt = payload.substring(t + 5);
+    int chk_name = tt.indexOf("Name=");
+    tt = tt.substring(0, chk_name);
+  time_get = tt;
+
+  // Fname + Lname
+  int name = payload.indexOf("Name=");
+  String extractedname = payload.substring(name + 5);
+    int bfIndex = extractedname.indexOf("st_bf");
+    extractedname = extractedname.substring(0, bfIndex);
+  Name = extractedname;
+
+  // TIME_BF
+  int stbf = payload.indexOf("st_bf=");
+    String stbfget = payload.substring(stbf + 6);
+    st_bf = stbfget.toInt();
+  int bf = payload.indexOf("BF=");
+  String extractedbf = payload.substring(bf + 3);
+    int lunIndex = extractedbf.indexOf("st_lun");
+    extractedbf = extractedbf.substring(0, lunIndex);
+  BF = extractedbf;
+
+  // TIME_LUN
+  int stlun = payload.indexOf("st_lun=");
+    String stlunget = payload.substring(stlun + 7);
+    st_lun = stlunget.toInt();
+  int lun = payload.indexOf("LUN=");
+  String extractedlun = payload.substring(lun + 4);
+    int dnIndex = extractedlun.indexOf("st_dn");
+    extractedlun = extractedlun.substring(0, dnIndex);
+  LUN = extractedlun;
+
+  // TIME_DIN
+  int stdn = payload.indexOf("st_dn=");
+    String stdnget = payload.substring(stdn + 6);
+    st_dn = stdnget.toInt();
+  int dn = payload.indexOf("DN=");
+  String extracteddn = payload.substring(dn + 3);
+    int bbIndex = extracteddn.indexOf("st_bb");
+    extracteddn = extracteddn.substring(0, bbIndex);
+  DN = extracteddn;
+
+  // TIME_BB
+  int stbb = payload.indexOf("st_bb=");
+    String stbbget = payload.substring(stbb + 6);
+    st_bb = stbbget.toInt();
+  int bb = payload.indexOf("BB=");
+  String extractedbb = payload.substring(bb + 3);
+  BB = extractedbb;
+}
+
+// Design layout
+void layout() {
+  tft.drawFastHLine(0, 0, tft.width(), ST7735_WHITE);
+  tft.drawFastVLine(0, 0, tft.height(), ST7735_WHITE);
+  tft.drawFastVLine(127, 0, tft.height(), ST7735_WHITE);
+  tft.drawFastHLine(1, 25, tft.width(), ST7735_WHITE); 
+  tft.drawFastHLine(1, 120, tft.width(), ST7735_WHITE);
+  tft.drawFastHLine(1, 159, tft.width(), ST7735_WHITE);
+}
+
+// Text alert medicine
+void txt_stt_medic() {
+  tft.setTextColor(ST77XX_GREEN);
+  tft.setTextSize(2);
+  tft.setCursor(12, 138);
+  tft.print("Medicine!");
+  delay(1000 * 10);
+}
+
+//TFT text from database
+void tft_text() {
+  tft.setTextSize(1);
+  tft.setCursor(6, 5);
+  tft.setTextColor(ST77XX_YELLOW);
+  tft.print(Name);
+  tft.setCursor(15, 30);
+  tft.print("Next medicine is");
+  tft.setCursor(47, 125);
+  tft.print("Status");
+
+  tft.setTextSize(3);
+  tft.setTextColor(ST77XX_GREEN);
+
+  if (time_get <= BF && st_bf == 1 || time_get >= BF && st_bf == 1 && st_bb == 0) {
+    tft.setCursor(40, 50);
+    tft.print("BBF");
+    tft.setCursor(20, 85);
+    tft.print(BF);
+    if (time_get == BF) {
+      txt_stt_medic();
+    }
+  } else if (time_get <= LUN && st_lun == 1 || time_get >= LUN && st_lun == 1 && st_bf == 0) {
+    tft.setCursor(40, 50);
+    tft.print("LUN");
+    tft.setCursor(20, 85);
+    tft.print(LUN);
+    if (time_get == LUN) {
+      txt_stt_medic();
+    }
+  } else if (time_get <= DN && st_dn == 1 || time_get >= DN && st_dn == 1 && st_lun == 0) {
+    tft.setCursor(40, 50);
+    tft.print("DNR");
+    tft.setCursor(20, 85);
+    tft.print(DN);
+    if (time_get == DN) {
+      txt_stt_medic();
+    }
+  } else if (time_get <= BB && st_bb == 1 || time_get >= BB && st_bb == 1 && st_dn == 0) {
+    tft.setCursor(40, 50);
+    tft.print("BED");
+    tft.setCursor(20, 85);
+    tft.print(BB);
+    if (time_get == BB) {
+      txt_stt_medic();
+    }
+  } else if(BF == "" || LUN == "" || DN == "" || BB == "") {
+    tft.setTextColor(ST77XX_RED);
+    tft.setCursor(40, 50);
+    tft.print("ERR");
+    tft.setCursor(40, 85);
+    tft.print(httpCode);
+    tft.setTextSize(2);
+    tft.setCursor(5, 138);
+    tft.print("CONNECTING");
+  } else { // BF < BB < time_get
+    tft.setCursor(40, 50);
+    tft.print("BBF");
+    tft.setCursor(20, 85);
+    tft.print(BF);
+    if (time_get == BF) {
+      txt_stt_medic();
+    }
+  }
+}

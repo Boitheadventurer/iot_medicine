ce6406f06fbfd9600509ea68d535d7df7c496ba7
Boitheadventurer <165980784+Boitheadventurer@users.noreply.github.com>
2024-05-16 14:09:35 +0700

    Create servo_test.ino

---
+++ servo_test.ino
@@ -0,0 +1,28 @@
+#include <Servo.h>
+Servo myservo;
+String Input;
+
+void setup() {
+  Serial.begin(115200);
+  myservo.attach(D4);
+  delay(150);
+}
+
+void loop() {
+  /*
+  if (Serial.available()) {
+    Input = Serial.readString();
+    Serial.print("Set Servo to : "); Serial.println(Input);
+    delay(150);
+    myservo.write(Input.toInt());
+    delay(3000);
+  }
+  delay(150);
+  */
+
+  for (int x = 0; x <= 180; x += 16) {
+    myservo.write(x);
+    Serial.println(x);
+    delay(5000);
+  }
+}

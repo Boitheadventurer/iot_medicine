bde7936c87e73b49832e5cbbddb03ec5806d24f3
Boitheadventurer <165980784+Boitheadventurer@users.noreply.github.com>
2024-05-17 16:40:59 +0700

    Update Control.ino

--- Control.ino
+++ Control.ino
@@ -28,7 +28,7 @@ int httpCode;
 
 //***GET***//
 int UserID; // UserID
-String fullname; // Fname + Lname
+String Fullname; // Fname + Lname
 String time_get;
 
 // Time meal
@@ -67,8 +67,11 @@ void setup() {
   Serial.print("Connect Code: "); Serial.println(httpCode);
   Serial.println(LINE.getVersion());
   LINE.setToken(LINE_TOKEN);
-  LINE.notify("SETUP COMPLETE");
+  //LINE.notify("SETUP COMPLETE");
   //Serial.print("payload: ");    Serial.println(payload);
+  Serial.print("UserID is "); Serial.println(UserID);
+  Serial.print("Fullname is "); Serial.println(Fullname);
+  Serial.print("Time is "); Serial.println(time_get);
   Serial.println("----------SETUP CONTROL.INO READY--------------");
   delay(250);
 }
@@ -130,19 +133,19 @@ void condition_GET() {
     String userIDString = payload.substring(index + 7);
     UserID = userIDString.toInt();
 
-  // GET fullname
-  int n = payload.indexOf("fullname=");
-    String nn = payload.substring(n + 9);
-    int substr_nn = nn.indexOf("UserID=");
-    nn = nn.substring(0, substr_nn);
-  fullname = nn;
-
   // GET RealTime from PHP
   int t = payload.indexOf("time=");
     String tt = payload.substring(t + 5);
-    int substr_tt = tt.indexOf("fullname=");
+    int substr_tt = tt.indexOf("UserID=");
     tt = tt.substring(0, substr_tt);
   time_get = tt;
+
+  // GET Fullname
+  int n = payload.indexOf("Fullname=");
+    String nn = payload.substring(n + 9);
+    int substr_nn = nn.indexOf("bf_time");
+    nn = nn.substring(0, substr_nn);
+  Fullname = nn;
   
   // GET bf_time
   int bf = payload.indexOf("bf_time=");
@@ -274,7 +277,7 @@ void condition_CHECK() {
       Serial.println("Take medicine failed!");
       status = "'failed'";
       LINE.notify("ผู้ป่วยไม่ได้รับยาในเวลาที่กำหนด!");
-      LINE.notifyPicture(fullname, "https://www.shareicon.net/data/256x256/2015/09/15/101562_incorrect_512x512.png");
+      LINE.notifyPicture(Fullname, "https://www.shareicon.net/data/256x256/2015/09/15/101562_incorrect_512x512.png");
       condition_POST();
       k = 0;
     }
@@ -282,7 +285,7 @@ void condition_CHECK() {
     Serial.println("Take medicine success!");
     status = "'success'";
     LINE.notify("ผู้ป่วยได้รับยาในเวลาที่กำหนด สำเร็จ!");
-    LINE.notifyPicture(fullname, "https://cdn-icons-png.flaticon.com/512/4436/4436481.png");
+    LINE.notifyPicture(Fullname, "https://cdn-icons-png.flaticon.com/512/4436/4436481.png");
     condition_POST();
     k = 0;
   }

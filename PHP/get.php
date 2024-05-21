5483b71a9c90fe4a336c11cc2477718bb7e59a8c
Boitheadventurer <165980784+Boitheadventurer@users.noreply.github.com>
2024-05-21 09:54:11 +0700

    Update get.php

--- PHP/get.php
+++ PHP/get.php
@@ -30,7 +30,7 @@ foreach($conn->query("SELECT * FROM `tb_data_bf` WHERE `id` = $a") as $z) {
     if ($bf_medic4 == "") {
         $bf_medic4 = "NULL";
     }
-    echo "bf_time=" . $bf_time;
+    echo "\nbf_time=" . $bf_time;
     echo "\nbf_medic1=" . $bf_medic1;
     echo "\nbf_medic2=" . $bf_medic2;
     echo "\nbf_medic3=" . $bf_medic3;
@@ -56,7 +56,7 @@ foreach($conn->query("SELECT * FROM `tb_data_lunch` WHERE `id` = $a") as $z) {
     if ($lun_medic4 == "") {
         $lun_medic4 = "NULL";
     }
-    echo "\n\nlun_time=" . $lun_time;
+    echo "\nlun_time=" . $lun_time;
     echo "\nlun_medic1=" . $lun_medic1;
     echo "\nlun_medic2=" . $lun_medic2;
     echo "\nlun_medic3=" . $lun_medic3;
@@ -82,7 +82,7 @@ foreach($conn->query("SELECT * FROM `tb_data_dn` WHERE `id` = $a") as $z) {
     if ($dn_medic4 == "") {
         $dn_medic4 = "NULL";
     }
-    echo "\n\ndn_time=" . $dn_time;
+    echo "\ndn_time=" . $dn_time;
     echo "\ndn_medic1=" . $dn_medic1;
     echo "\ndn_medic2=" . $dn_medic2;
     echo "\ndn_medic3=" . $dn_medic3;
@@ -108,7 +108,7 @@ foreach($conn->query("SELECT * FROM `tb_data_bb` WHERE `id` = $a") as $z) {
     if ($bb_medic4 == "") {
         $bb_medic4 = "NULL";
     }
-    echo "\n\nbb_time=" . $bb_time;
+    echo "\nbb_time=" . $bb_time;
     echo "\nbb_medic1=" . $bb_medic1;
     echo "\nbb_medic2=" . $bb_medic2;
     echo "\nbb_medic3=" . $bb_medic3;

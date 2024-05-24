6105cf296c69f231b02db136e1cd15c5c468b74a
BigBoy The Code Adventurer <165980784+Boitheadventurer@users.noreply.github.com>
2024-05-24 09:51:35 +0700

    Update Output.ino

--- C++/Output.ino
+++ C++/Output.ino
@@ -4,12 +4,12 @@
 #include <ESP8266HTTPClient.h>
 
 /*2.4G*/
-const char* ssid = "SSID"; // Wi-Fi SSID
-const char* password = "PASSWORD"; // Wi-Fi password
+const char* ssid = "CTN floor 2 teacher"; // Wi-Fi SSID
+const char* password = "ctnphrae"; // Wi-Fi password
 
 // URL by file PHP (http://(IP4)/(folder)(file.php))
 
-String GETURL = "http://IP4/Medic/tft.php";
+String GETURL = "http://192.168.10.41/Medic/tft.php";
 //String GETURL = "http://medicinectn2555.000webhostapp.com/tft.php";
 
 WiFiClient client;

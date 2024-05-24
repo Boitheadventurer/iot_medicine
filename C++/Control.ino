bbbc00de612ee0652c66d45df0b2d49ab4160df6
BigBoy The Code Adventurer <165980784+Boitheadventurer@users.noreply.github.com>
2024-05-24 11:35:10 +0700

    Update Control.ino

--- C++/Control.ino
+++ C++/Control.ino
@@ -10,8 +10,8 @@ Servo myservo;
 int x, i, k, val;
 
 /*2.4G*/
-const char* ssid = "CTN floor 2 teacher"; // Wi-Fi SSID
-const char* password = "ctnphrae"; // Wi-Fi password
+const char* ssid = "SSID"; // Wi-Fi SSID
+const char* password = "PASSWORD"; // Wi-Fi password
 
 const char* LINE_TOKEN = "QIChSQJdNBnK08VtgMSjRPhikDUQVGP3ikBPexwgQFU";
 

6558dbda34cd610eafca6ac67f1b3cfc34412b2a
BigBoy The Code Adventurer <165980784+Boitheadventurer@users.noreply.github.com>
2024-05-29 14:27:08 +0700

    Update Output.ino

--- C++/Output.ino
+++ C++/Output.ino
@@ -7,8 +7,8 @@
 #include <Wire.h>
 
 /*2.4G*/
-const char* ssid = "CTN floor 2 teacher"; // Wi-Fi SSID
-const char* password = "ctnphrae"; // Wi-Fi password
+const char* ssid = "SSID"; // Wi-Fi SSID
+const char* password = "PASSWORD"; // Wi-Fi password
 
 // URL by file PHP (http://(IP4)/(folder)(file.php))
 
